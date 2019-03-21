0db95764cb16b0d55010deb20ef956f6144f691e
Theo Weller <weller.theodor@gmail.com>
2019-03-21 14:39:43 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,13 +1,3 @@
 UPDATE characters
 SET species = "Martian"
 WHERE species = "cylon"
-
--- ORDER BY characters.id DESC
--- LIMIT 1;
--- ORDER BY SELECT MAX(field3) FROM table1
--- UPDATE characters
--- SET characters.species = "Martian";
--- WHERE
--- SELECT * FROM characters ORDER BY characters.id DESC LIMIT 1
--- SET characters.species = "Martian";
--- (SELECT MAX(characters.id) FROM characters)
