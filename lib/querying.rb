ccbb48c9c2f89abad659329f2c2b7c97b8be82a2
Theo Weller <weller.theodor@gmail.com>
2019-03-21 15:44:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) AS species_count
   FROM characters
-  WHERE MAX(species_count)
+  ORDER BY species_count DESC;
   "
 end
 
