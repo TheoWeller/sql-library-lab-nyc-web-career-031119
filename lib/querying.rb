9223d7c4abcfb8af111c43bdef979e6ba9b008bf
Theo Weller <weller.theodor@gmail.com>
2019-03-21 14:59:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,12 +11,11 @@ def select_name_and_motto_of_char_with_longest_motto
   ORDER BY motto_length DESC
   LIMIT 1;
   "
-
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)"
 end
 
 def select_name_and_series_subgenres_of_authors
