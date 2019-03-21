2449925d7473e81c3ff35a968c8fecb79cc1aca4
Theo Weller <weller.theodor@gmail.com>
2019-03-21 15:27:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,9 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-  FROM characters
+  FROM series
+  JOIN characters
+  ON characters.series_id = series.id
   WHERE species = 'human';"
 end
 
